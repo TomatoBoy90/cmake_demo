3e8f499199b3d9216f72fb6e43c047c5d8f5a230
13281861929 <2548737967@qq.com>
2022-03-07 15:14:19 +0800

    更新文件

---
+++ call_share/build/CMakeFiles/print_04.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/print_04.dir/main.cpp.o"
+  "CMakeFiles/print_04.dir/main.cpp.o.d"
+  "print_04"
+  "print_04.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/print_04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ call_share/build/CMakeFiles/print_04.dir/main.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/print_04.dir/main.cpp.o: /opt/cmake_test/call_share/main.cpp \
+ /usr/include/stdc-predef.h /opt/cmake_test/call_share/include/Print.h
